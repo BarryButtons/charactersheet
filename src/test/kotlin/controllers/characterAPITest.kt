356bbb48a5b4d6653edecb4cd3b78c8436d827d7
Blah <blah@gmail.com>
2022-12-06 19:07:03 +0000

    Add Tests

--- src/test/kotlin/controllers/characterAPITest.kt
+++ src/test/kotlin/controllers/characterAPITest.kt
@@ -1,4 +1,20 @@
 package controllers
 
+import models.Sheet
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+import java.io.File
+import kotlin.test.assertEquals
 class characterAPITest {
+
+    private var Grog: Sheet? = null
+    private var Anees: Sheet? = null
+    private var Davos: Sheet? = null
+    private var Ben: Sheet? =null
+
+    @BeforeEach
+    fun setup(){
+        Grog = Sheet( "Grog Strongjaw","Goliath","Barbarian",20,20,14,18,6,10,10)
+        Anees = Sheet("Anees","Halfling", )
+    }
 }
